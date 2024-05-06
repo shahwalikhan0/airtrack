0ce82f5931336e8c699ca50219ac29cb093c8611
Tassadaqq <tassadaqq.abid@gmail.com>
2024-05-06 21:25:13 +0500

    Booking functionality of corresponding user unsuccessful

--- Frontend/src/passenger/Booking.js
+++ Frontend/src/passenger/Booking.js
@@ -1,48 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useUserAuth } from "../general/constant";
+import { useNavigate } from "react-router-dom";
+import { Table } from "antd";
+import axios from "axios";
+import { bookingColumns, flightColumns, passengerColumns } from "../admin/constant";
 
 export const Booking = () => {
-  const isLogged = localStorage.getItem("isLogged");
+  const navigate = useNavigate();
+  const isUserLoggedIn = useUserAuth();
 
-  if(isLogged === 'false' || isLogged === null){
-    window.location.href = "/";
-  }
+  const [flights, setFlights] = useState([]);
+  const [passengers, setPassengers] = useState([]);
 
-    return (
-        <>
-        <body>
-    <main className="flight-main">
-    <header>
-      <h1>Airtrack - Bookings</h1>
-    </header>
-      <table>
-        <thead>
-          <tr>
-            <th>Flight Number</th>
-            <th>Departure Time</th>
-            <th>Arrival Time</th>
-            <th>Destination</th>
-            <th>Seats Booked</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>123</td>
-            <td>10:00 AM</td>
-            <td>12:00 PM</td>
-            <td>New York</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>456</td>
-            <td>2:00 PM</td>
-            <td>4:00 PM</td>
-            <td>Los Angeles</td>
-            <td>5</td>
-          </tr>
-        </tbody>
-      </table>
+  useEffect(() => {
+    if (!isUserLoggedIn) {
+      navigate("/");
+    }
+  }, [isUserLoggedIn, navigate]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/flight")
+      .then((res) => {
+        setFlights(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    axios
+      .get("http://localhost:3001/passenger")
+      .then((res) => {
+        setPassengers(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <main className="booking-main">
+      <div className="booking-container-header">
+        <h1 style={{ width: "50%" }}>Airtrack - Bookings</h1>
+      </div>
+      <Table
+        bordered
+        columns={bookingColumns(flights, passengers)} // Pass flights and passengers to bookingColumns
+        dataSource={flights} 
+      />
     </main>
-  </body>
-        </>
-    )
-}
+  );
+};
